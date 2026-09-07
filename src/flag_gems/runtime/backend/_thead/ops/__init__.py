22595cd1fe27e15decf6bf67f6bc31c2ae4a0a34
chx7514 <49188554+chx7514@users.noreply.github.com>
2026-09-07 12:04:46 +0800

    [KernelGen][Thead] Add special_gammaln_out T-Head specialized operator (#330)

--- src/flag_gems/runtime/backend/_thead/ops/__init__.py
+++ src/flag_gems/runtime/backend/_thead/ops/__init__.py
@@ -50,6 +50,7 @@ from .special_chebyshev_polynomial_w import (
 from .special_erfc import special_erfc
 from .special_erfinv import special_erfinv, special_erfinv_, special_erfinv_out
 from .special_gammainc import special_gammainc
+from .special_gammaln_out import special_gammaln_out
 from .special_hermite_polynomial_h import (
     special_hermite_polynomial_h,
     special_hermite_polynomial_h_tensor_tensor,
@@ -110,6 +111,7 @@ __all__ = [
     "special_erfinv_",
     "special_erfinv_out",
     "special_gammainc",
+    "special_gammaln_out",
     "special_hermite_polynomial_h",
     "special_hermite_polynomial_h_tensor_tensor",
     "special_round",
